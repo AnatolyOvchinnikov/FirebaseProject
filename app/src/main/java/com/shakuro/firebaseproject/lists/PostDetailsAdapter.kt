88756af46686b8fbb39d32178788b4795e47579d
Anatoly Ovchinnikov <aovchinnikov@shakuro.com>
2018-05-31 11:01:31 +0300

    Added user icon at post details

--- app/src/main/java/com/shakuro/firebaseproject/lists/PostDetailsAdapter.kt
+++ app/src/main/java/com/shakuro/firebaseproject/lists/PostDetailsAdapter.kt
@@ -5,13 +5,15 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.firebase.ui.storage.images.FirebaseImageLoader
+import com.google.firebase.storage.FirebaseStorage
 import com.shakuro.firebaseproject.R
 import com.shakuro.firebaseproject.entity.CommentItem
 import com.shakuro.firebaseproject.extentions.timestampToDate
 import kotlinx.android.synthetic.main.comment_item_layout.view.*
-
 
 
 class PostDetailsAdapter(val context: Context, options: FirebaseRecyclerOptions<Any>) : FirebaseRecyclerAdapter<Any, RecyclerView.ViewHolder>(options) {
@@ -35,7 +37,18 @@ class PostDetailsAdapter(val context: Context, options: FirebaseRecyclerOptions<
                 this.userTextView.text = commentItem.userName
                 this.messageTextView.text = commentItem.message
                 this.messageTimestamp.text = (commentItem.timestamp as Long).timestampToDate()
+                loadAvatar(commentItem.userId)
             }
+        }
+
+        private fun loadAvatar(userId: String?) {
+            val storageRef = FirebaseStorage.getInstance().reference
+            val pathReference = storageRef.child("users/${userId}/avatar.jpg")
+            Glide.with(context)
+                    .using(FirebaseImageLoader())
+                    .load(pathReference)
+                    .placeholder(R.mipmap.ic_launcher)
+                    .into(itemView.userImageView)
         }
     }
 }
